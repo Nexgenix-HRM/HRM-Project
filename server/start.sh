c095eb091613c6bebf3d26b4f2c07a9d10e1d627
Md Monir Hossain <mdmonirhossain1165@gmail.com>
2026-02-28 21:58:25 +0600

    storage updated

--- server/start.sh
+++ server/start.sh
@@ -2,6 +2,10 @@
 echo "Running migrations and seeding..."
 php artisan migrate --seed --force
 
+# Create storage link
+echo "Creating storage link..."
+php artisan storage:link
+
 # Start Apache
 echo "Starting Apache..."
 apache2-foreground
